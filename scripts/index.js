1c28f6d512f030bec931262195a4547c9e876086
Andrei <olgi83@yandex.ru>
2022-01-20 19:12:03 +0300

    комментарий к коммиту

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
-let open = document.querySelector('.profile__edit-button');
-let block = document.querySelector('.popup');
-let close = document.querySelector('.popup__link');
+let openPopupButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closePopupButton = document.querySelector('.popup__link');
 
-open.addEventListener('click', myFunction);
+openPopupButton.addEventListener('click', openPopup);
 
 
-function myFunction() {
-    block.classList.remove('popup_opened');
+function openPopup() {
+    popup.classList.add('popup_opened');
 }
 
 
 
-close.addEventListener('click', logo);
+closePopupButton.addEventListener('click', closePopup);
 
-function logo() {
-    block.classList.add('popup_opened');
+function closePopup() {
+    popup.classList.remove('popup_opened');
 }
 
 
 
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input');
-let jobInput = document.querySelector('.popup__input2');
+let nameInput = document.querySelector('.popup__input_place_name');
+let jobInput = document.querySelector('.popup__input_place_job');
 
 let title = document.querySelector('.profile__title');
 
@@ -29,12 +29,12 @@ nameInput.value = title.textContent;
 
 
 let subTitle = document.querySelector('.profile__subtitle');
-jobInput.value = subTitle.textContent
+jobInput.value = subTitle.textContent;
 
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    logo();
+    closePopup();
     title.textContent = nameInput.value;
     subTitle.textContent = jobInput.value
 }
